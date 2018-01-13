ce22669edac5329cca3f4771f8117371e8f8be74
josephcv <35392930+josephcv@users.noreply.github.com>
2018-01-13 13:15:56 +0530

    Add files via upload

---
+++ Assignment_less1.py
@@ -0,0 +1,48 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+# first program in jupyter notebook
+
+print("Welcome to Python")
+
+
+# In[2]:
+
+
+# Create a list based on some conditions
+
+num= range(2000,3200)
+l=[]
+for i in num:
+    if i%7 == 0 and i%5 != 0:
+       l.append(i)
+print(l)
+
+
+
+
+# In[3]:
+
+
+# Get first and last name and print in reverse
+
+first_nm = input("Enter your first name")
+last_nm= input("Enter your last name")
+print(last_nm, first_nm)
+
+
+# In[4]:
+
+
+# Calculate volume of a sphere
+
+radius = 12
+pi = 3.14
+
+Vol = 4/3 * pi * radius**3
+
+print("Volume of sphere is", Vol)
+
